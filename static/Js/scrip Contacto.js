3de6605a9d709c53356932e05317a7518ddf5be9
Arcángel <axel@bazan.com.ar>
2024-05-23 11:17:11 -0300

    Se modifica  el html contacto y se agrega un scrip Contacto para validar campos.

---
+++ static/Js/scrip Contacto.js
@@ -0,0 +1,89 @@
+const formContacto=document.getElementById("formContacto");
+
+const inputEmail=document.getElementById("emailContacto");
+const inputNombre=document.getElementById("nombreContacto");
+const inputAsunto=document.getElementById("asuntoContacto");
+const inputMensaje=document.getElementById("mensajeContacto");
+
+
+const parrafoEmail=document.getElementById("errorEmail");
+const parrafoNombre=document.getElementById("errorNombre");
+const parrafoAsunto=document.getElementById("errorAsunto");
+const parrafoMensaje=document.getElementById("errorMensaje");
+
+// console.log(inputNombre);
+
+formContacto.addEventListener("submit",e=>{
+e.preventDefault();
+
+let warningEmail="";
+let warningNombre="";
+let warningAsunto="";
+let warningMensaje="";
+
+let valorEmail=false;
+let valorNombre=false;
+let valorAsunto=false;
+let valorMensaje=false;
+
+parrafoEmail.innerHTML="";
+parrafoNombre.innerHTML="";
+parrafoAsunto.innerHTML="";
+parrafoMensaje.innerHTML="";
+
+
+let regexEmail=/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+let contador = 4;
+
+// VALIDA LOS CAMPOS.
+
+if(!regexEmail.test(inputEmail.value)){
+    warningEmail=`El email no es valido.`
+    valorEmail=true;
+    contador--;
+}
+
+if(inputNombre.value.length<3){
+   warningNombre=`El nombre es corto.`
+   valorNombre=true;
+   contador--;
+}
+
+if(inputAsunto.value.length<1){
+    warningAsunto=`El mensaje debe incluir un Asunto.`
+    valorAsunto=true;
+    contador--;
+ }
+
+if(inputMensaje.value.length<1){
+    warningMensaje=`El mensaje esta vacio.`
+    valorMensaje=true;
+    contador--;
+ }
+
+// ESCRIBE POR PANTALLA LOS MENSAJES EN LOS "P".
+
+if(valorEmail){
+    parrafoEmail.innerHTML=warningEmail;
+}
+
+if(valorNombre){
+    parrafoNombre.innerHTML=warningNombre;
+}
+
+if(valorAsunto){
+    parrafoAsunto.innerHTML=warningAsunto;
+}
+
+if(valorMensaje){
+    parrafoMensaje.innerHTML=warningMensaje;
+}
+
+// BORRA TODO EL FORMULARIO SI TODOS LOS CAMPOS ESTAN BIEN.
+
+if(!valorEmail && !valorNombre && !varlorAsunto && !valorMensaje && contador == 4){
+    formContacto.reset();
+}
+
+})
