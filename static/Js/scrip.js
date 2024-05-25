8c74d52d883f05a7b4b4d0eadb050ed4a8abbf3c
Arcángel <axel@bazan.com.ar>
2024-05-25 02:43:21 -0300

    Se agrega validador de Fecha para que no sea menor a la actual.

--- static/Js/scrip.js
+++ static/Js/scrip.js
@@ -1,11 +1,13 @@
 const formReserva = document.getElementById("formReserva");
 
+const inputFecha = document.getElementById("fechaReserva");
 const inputHora = document.getElementById("horaReserva");
 const inputComensales = document.getElementById("comensalesReserva");
 const inputEmail = document.getElementById("emailReserva");
 const inputNombre = document.getElementById("nombreReserva");
 const inputLocal = document.getElementById("localReserva");
 
+const parrafoFecha = document.getElementById("errorFecha");
 const parrafoHora = document.getElementById("errorHora");
 const parrafoComensales = document.getElementById("errorComensales");
 const parrafoEmail = document.getElementById("errorEmail");
@@ -17,18 +19,21 @@ const parrafoLocal = document.getElementById("errorLocal");
 formReserva.addEventListener("submit", e => {
     e.preventDefault();
 
+    let warningFecha = "";
     let warningHora = "";
     let warningComensales = "";
     let warningEmail = "";
     let warningNombre = "";
     let warningLocal = "";
 
+    let valorFecha = false;
     let valorHora = false;
     let valorComensales = false;
     let valorEmail = false;
     let valorNombre = false;
     let valorLocal = false;
 
+    parrafoFecha.innerHTML = "";
     parrafoHora.innerHTML = "";
     parrafoComensales.innerHTML = "";
     parrafoEmail.innerHTML = "";
@@ -38,6 +43,49 @@ formReserva.addEventListener("submit", e => {
     let regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 
     // VALIDA LOS CAMPOS.
+
+    let fechaAhora = new Date();
+    let fechatexto = fechaAhora.toISOString().split("T");
+    let arregloFechaIntroducida = inputFecha.value.split("-");
+    let arregloFechaActual= fechatexto[0].split("-");
+
+    // prompt((parseInt(arregloFechaIntroducida[0]) < parseInt(arregloFechaActual[0])));
+
+    if ((parseInt(arregloFechaIntroducida[0]) < parseInt(arregloFechaActual[0]))){
+        // prompt(parseInt(arregloFechaIntroducida[0]))
+        // prompt(parseInt(arregloFechaActual[0]))
+        warningFecha = `La fecha no es valida.`
+        valorFecha = true;
+
+    }else if (arregloFechaIntroducida[0] == arregloFechaActual[0]){
+            // prompt(arregloFechaIntroducida[0] = arregloFechaActual[0])
+        if (arregloFechaIntroducida[1] < arregloFechaActual[1]){
+            // prompt("1")
+            warningFecha = `La fecha no es valida.`
+            valorFecha = true;
+        }else if(arregloFechaIntroducida[2] < arregloFechaActual[2]){
+            // prompt("2")
+            warningFecha = `La fecha no es valida.`
+            valorFecha = true;
+        }
+    }
+
+    //Explicacion de lo de arriba
+        // si añoAgregado > añoActual
+        //     valido
+        // si añoAgregado = añoActual
+        //     si mesAgregado > mesActual
+        //         valido
+        //     si mesAgregado = mesActual
+        //         si diaAgregado >= diaActual
+        //             valido
+        //         si diaAgregado < diaActual
+        //             negar
+        //     si mesAgregado < mesActual
+        //         negar
+        // si añoAgregado < añoActual
+        //     negar
+
 
     if (!(inputHora.value == "09:00" ||
         inputHora.value == "09:30" ||
@@ -85,6 +133,13 @@ formReserva.addEventListener("submit", e => {
 
     // ESCRIBE POR PANTALLA LOS MENSAJES EN LOS "P".
 
+    if (valorFecha) {
+        parrafoFecha.innerHTML = warningFecha;
+    } else {
+        // parrafo.innerHTML="Enviado";
+        // formRegister.reset();
+    }
+
     if (valorHora) {
         parrafoHora.innerHTML = warningHora;
     } else {
@@ -122,7 +177,7 @@ formReserva.addEventListener("submit", e => {
 
     // BORRA TODO EL FORMULARIO SI TODOS LOS CAMPOS ESTAN BIEN.
 
-    if (!valorEmail && !valorNombre && !valorLocal && !valorComensales && !valorHora) {
+    if (!valorEmail && !valorNombre && !valorLocal && !valorComensales && !valorHora && !valorFecha) {
         formReserva.reset();
     }
 
